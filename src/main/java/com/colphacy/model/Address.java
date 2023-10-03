d8828660a174cf2a4710cd37a37ea2274f002334
cqtruong <truongquangchu.tqc@gmail.com>
2023-10-03 21:40:34 +0700

    CP-30: Using ddl auto without migrations

--- src/main/java/com/colphacy/model/Address.java
+++ src/main/java/com/colphacy/model/Address.java
@@ -5,7 +5,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
