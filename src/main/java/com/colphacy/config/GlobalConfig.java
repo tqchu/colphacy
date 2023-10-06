ffc635753e329cddef1762d2d677f5d42a8173c0
cqtruong <truongquangchu.tqc@gmail.com>
2023-10-06 19:46:10 +0700

    CP-39: [Draft] Allow cors

--- src/main/java/com/colphacy/config/GlobalConfig.java
+++ src/main/java/com/colphacy/config/GlobalConfig.java
@@ -4,6 +4,8 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class GlobalConfig {
@@ -13,5 +15,17 @@ public class GlobalConfig {
                 .info(new Info()
                         .title("API Doc for Colphacy application")
                         .version("1.0"));
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000", "http://localhost:3001")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
     }
 }
