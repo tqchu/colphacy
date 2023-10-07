ce4b380ae6d798380470b944fc5b46d0dd377b3f
spartan-duyle <duy.le@c0x12c.com>
2023-10-07 11:06:03 +0700

    throw error message if user not found

--- src/main/java/com/colphacy/payload/request/LoginRequest.java
+++ src/main/java/com/colphacy/payload/request/LoginRequest.java
@@ -10,11 +10,11 @@ import javax.validation.constraints.Size;
 public class LoginRequest {
     @NotBlank
     @NotNull
-    @Size(min = 6)
+    @Size(min = 6, message = "Tên tài khoản có độ dài ít nhất là 6 ký tự")
     private String username;
 
     @NotBlank
     @NotNull
-    @Size(min=8)
+    @Size(min=8, message = "Mật khẩu có độ dài ít nhất là 8 ký tự")
     private String password;
 }
