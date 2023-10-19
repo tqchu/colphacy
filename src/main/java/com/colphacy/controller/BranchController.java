d87bfac1c0178dcc5b5cfe66530c02de37536f6a
cqtruong <truongquangchu.tqc@gmail.com>
2023-10-19 23:28:59 +0700

    CP-51: Allow branches searching by province Abort the api /provinceSlug/districtSlug, use province and district as optional request param instead

--- src/main/java/com/colphacy/controller/BranchController.java
+++ src/main/java/com/colphacy/controller/BranchController.java
@@ -25,7 +25,7 @@ public class BranchController {
     @Operation(summary = "Get all provinces along with their slugs", security = {@SecurityRequirement(name = "bearer-key")})
     @GetMapping("/provinces")
     public List<SlugDTO> getAllProvinces() {
-        return branchService.getAllProvinces();
+        return branchService.getBranches();
     }
 
     @Operation(summary = "Get all districts of a province along with their slugs", security = {@SecurityRequirement(name = "bearer-key")})
@@ -34,40 +34,29 @@ public class BranchController {
         return branchService.getAllDistricts(slug);
     }
 
-    @Operation(summary = "Get all branches in a district", security = {@SecurityRequirement(name = "bearer-key")})
-    @GetMapping("/{provinceSlug}/{districtSlug}")
-    public PageResponse<BranchListViewDTO> getBranchesInDistrict(@PathVariable String provinceSlug,
-                                                                 @PathVariable String districtSlug,
-                                                                 @RequestParam(required = false, defaultValue = "0")
-                                                                 @Size(min = 0, message = "Số bắt đầu phải là số không âm") int offset,
-                                                                 @RequestParam(required = false)
-                                                                 @Size(min = 1, message = "Số lượng hàng mỗi trang phải lớn hơn 0") Integer limit) {
-        if (limit == null) {
-            limit = defaultPageSize;
-        }
-        return branchService.getBranchesInDistrict(provinceSlug, districtSlug, offset, limit);
-    }
-
     @Operation(summary = "Get all branches by keyword", security = {@SecurityRequirement(name = "bearer-key")})
     @GetMapping("/search")
     public PageResponse<BranchListViewDTO> getBranchesByKeyword(String keyword,
                                                                 @RequestParam(required = false, defaultValue = "0")
                                                                 @Size(min = 0, message = "Số bắt đầu phải là số không âm") int offset,
-                                                        @RequestParam(required = false, defaultValue = "5")
-                                                        @Size(min = 1, message = "Số lượng giới hạn phải lớn hơn 0") int limit) {
+                                                                @RequestParam(required = false, defaultValue = "5")
+                                                                    @Size(min = 1, message = "Số lượng giới hạn phải lớn hơn 0") int limit) {
         return branchService.getBranchesByKeyword(keyword, offset, limit);
     }
 
     @Operation(summary = "Get all branches", security = {@SecurityRequirement(name = "bearer-key")})
     @GetMapping("")
-    public PageResponse<BranchListViewDTO> getAllBranches(@RequestParam(required = false, defaultValue = "0")
-                                                          @Size(min = 0, message = "Số bắt đầu phải là số không âm") int offset,
-                                                          @RequestParam(required = false)
-                                                          @Size(min = 1, message = "Số lượng hàng mỗi trang phải lớn hơn 0") Integer limit) {
+    public PageResponse<BranchListViewDTO> getAllBranches(
+            @RequestParam(required = false) String province,
+            @RequestParam(required = false) String district,
+            @RequestParam(required = false, defaultValue = "0")
+            @Size(min = 0, message = "Số bắt đầu phải là số không âm") int offset,
+            @RequestParam(required = false)
+            @Size(min = 1, message = "Số lượng hàng mỗi trang phải lớn hơn 0") Integer limit) {
         if (limit == null) {
             limit = defaultPageSize;
         }
-        return branchService.getAllProvinces(offset, limit);
+        return branchService.getBranches(province, district, offset, limit);
     }
 
 }
