87bd2c11136228aa450bfd5c8b4a46a2d6150aa4
spartan-duyle <93651748+spartan-duyle@users.noreply.github.com>
2023-10-22 23:19:49 +0700

    CP-59: employee change password

--- src/main/java/com/colphacy/controller/EmployeeController.java
+++ src/main/java/com/colphacy/controller/EmployeeController.java
@@ -1,11 +1,17 @@
 package com.colphacy.controller;
 
 import com.colphacy.dto.EmployeeDetailDTO;
+import com.colphacy.exception.InvalidFieldsException;
+import com.colphacy.model.Employee;
+import com.colphacy.payload.request.ChangePasswordRequest;
 import com.colphacy.service.EmployeeService;
+import com.colphacy.validation.ChangePasswordValidator;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -13,8 +19,19 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/api/employees")
 public class EmployeeController {
-    @Autowired
     private EmployeeService employeeService;
+
+    private ChangePasswordValidator changePasswordValidator;
+
+    @Autowired
+    public void setEmployeeService(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @Autowired
+    public void setChangePasswordValidator(ChangePasswordValidator changePasswordValidator) {
+        this.changePasswordValidator = changePasswordValidator;
+    }
 
     @Operation(summary = "Employee get profile", security = {@SecurityRequirement(name = "bearer-key")})
     @PreAuthorize("#id == principal.id")
@@ -30,6 +47,16 @@ public class EmployeeController {
         return employeeService.save(id, employeeDetailDTO);
     }
 
+    @Operation(summary = "Employee change password", security = {@SecurityRequirement(name = "bearer-key")})
+    @PutMapping("/change-password")
+    public void changePassword(@Valid @RequestBody ChangePasswordRequest request, @AuthenticationPrincipal Employee employee, Errors errors) {
+        // Validate the request
+        changePasswordValidator.validate(request, errors);
 
+        if (errors.hasErrors()) {
+            throw InvalidFieldsException.fromFieldError("confirmPassword", "Mật khẩu xác nhận không trùng khớp");
+        }
 
+        employeeService.changePassword(employee.getId(), request);
+    }
 }
