72648015d1000c2408ee415fc7e37ce2c41b98f3
cqtruong <truongquangchu.tqc@gmail.com>
2023-10-25 10:31:15 +0700

    CP-49: Fix bug log out for employee

--- src/main/java/com/colphacy/service/AuthenticationService.java
+++ src/main/java/com/colphacy/service/AuthenticationService.java
@@ -3,13 +3,11 @@ package com.colphacy.service;
 import com.colphacy.payload.request.LoginRequest;
 import com.colphacy.payload.response.CustomerLoginResponse;
 import com.colphacy.payload.response.EmployeeLoginResponse;
-import com.colphacy.payload.response.LogoutResponse;
 
 public interface AuthenticationService {
     EmployeeLoginResponse loginByEmployee(LoginRequest loginRequest);
 
     CustomerLoginResponse loginByCustomer(LoginRequest loginRequest);
 
-    LogoutResponse logout(String authorization, Long principalId);
-
+    void logout(String authorization, Long principalId);
 }
