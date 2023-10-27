7a448a101a032ef0ae86b496b8b6faad13d0fac0
cqtruong <truongquangchu.tqc@gmail.com>
2023-10-27 22:23:48 +0700

    CP-54: Change request mapping for location controller

--- src/main/java/com/colphacy/controller/LocationController.java
+++ src/main/java/com/colphacy/controller/LocationController.java
@@ -8,11 +8,13 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController("/api/location")
+@RestController
+@RequestMapping("/api/location")
 public class LocationController {
     @Autowired
     private LocationService locationService;
