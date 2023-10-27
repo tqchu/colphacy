8c24afff7eb2c202076c7d877775fc77f111de92
spartan-duyle <duy.le@c0x12c.com>
2023-10-28 00:38:43 +0700

    CP-74: BE API update unit

--- src/main/java/com/colphacy/service/UnitService.java
+++ src/main/java/com/colphacy/service/UnitService.java
@@ -1,7 +1,12 @@
 package com.colphacy.service;
 
 import com.colphacy.dto.unit.UnitDTO;
+import com.colphacy.model.Unit;
 
 public interface UnitService {
     UnitDTO create(UnitDTO unitDTO);
+
+    UnitDTO update(UnitDTO unitDTO);
+
+    UnitDTO findById(Long id);
 }
