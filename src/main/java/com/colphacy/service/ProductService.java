a4b4a6cf31f2ecfa3e581cb369c3c85940a8769f
Trương Quang Chứ <truongquangchu.tqc@gmail.com>
2023-11-09 23:31:43 +0700

    CP-81: Get product's detail

--- src/main/java/com/colphacy/service/ProductService.java
+++ src/main/java/com/colphacy/service/ProductService.java
@@ -1,7 +1,12 @@
 package com.colphacy.service;
 
 import com.colphacy.dto.product.ProductDTO;
+import com.colphacy.model.Product;
 
 public interface ProductService {
     ProductDTO create(ProductDTO productDTO);
+
+    ProductDTO findProductDTOById(Long id);
+
+    Product findById(Long id);
 }
