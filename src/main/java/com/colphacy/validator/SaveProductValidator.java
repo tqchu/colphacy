af3078b145f729a851f5bc88391f274f934a0b18
cqtruong <truongquangchu.tqc@gmail.com>
2023-11-11 10:24:10 +0700

    CP-83: Add constraint for images when for_sale

--- src/main/java/com/colphacy/validator/SaveProductValidator.java
+++ src/main/java/com/colphacy/validator/SaveProductValidator.java
@@ -2,6 +2,7 @@ package com.colphacy.validator;
 
 import com.colphacy.dto.product.ProductDTO;
 import com.colphacy.dto.product.ProductUnitDTO;
+import com.colphacy.model.ProductStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
@@ -21,14 +22,18 @@ public class SaveProductValidator implements Validator {
                 .filter(productUnit -> productUnit.getRatio() == 1)
                 .count();
         if (countRatioOne != 1) {
-            errors.rejectValue("productUnit.ratio", "ratio.invalid", "Phải có duy nhất 1 tỉ lệ quy đổi có giá trị bằng 1");
+            errors.rejectValue("productUnits", "ratio.invalid", "Phải có duy nhất 1 tỉ lệ quy đổi có giá trị bằng 1");
         }
 
         long countDefaultUnit = product.getProductUnits().stream()
                 .filter(ProductUnitDTO::isDefaultUnit)
                 .count();
         if (countDefaultUnit == 0) {
-            errors.rejectValue("productUnit.isDefaultUnit", "isDefaultUnit.invalid", "Phải có ít nhất 1 đơn vị tính được đăng bán");
+            errors.rejectValue("productUnits", "isDefaultUnit.invalid", "Phải có ít nhất 1 đơn vị tính được đăng bán");
+        }
+
+        if (product.getStatus() == ProductStatus.FOR_SALE && (product.getImages() == null || product.getImages().isEmpty())) {
+            errors.rejectValue("images", "image.required", "Phải thêm ảnh cho sản phẩm được đăng bán");
         }
     }
 }
