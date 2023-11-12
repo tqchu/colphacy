563348a76840df4a40d7978e6e489eb5d841f246
cqtruong <truongquangchu.tqc@gmail.com>
2023-11-12 22:23:48 +0700

    CP-80: Fix bug and add validations for saving products

--- src/main/java/com/colphacy/repository/IngredientRepository.java
+++ src/main/java/com/colphacy/repository/IngredientRepository.java
@@ -2,7 +2,11 @@ package com.colphacy.repository;
 
 import com.colphacy.model.Ingredient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
+    @Modifying
+    @Query("delete from Ingredient i where i.product.id = ?1")
     void deleteByProductId(Long productId);
 }
--- src/main/java/com/colphacy/repository/ProductUnitRepository.java
+++ src/main/java/com/colphacy/repository/ProductUnitRepository.java
@@ -2,8 +2,12 @@ package com.colphacy.repository;
 
 import com.colphacy.model.ProductUnit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductUnitRepository extends JpaRepository<ProductUnit, Long> {
+    @Modifying
+    @Query("delete from ProductUnit pu where pu.product.id = ?1")
     void deleteByProductId(Long productId);
 
 }
