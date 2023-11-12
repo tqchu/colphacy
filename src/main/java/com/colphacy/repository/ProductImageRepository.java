3c64bf2eebd632c4f48127108b15a13f6c0a9bb2
cqtruong <truongquangchu.tqc@gmail.com>
2023-11-12 22:23:48 +0700

    CP-80: Fix bug and add validations for saving products

--- src/main/java/com/colphacy/repository/ProductImageRepository.java
+++ src/main/java/com/colphacy/repository/ProductImageRepository.java
@@ -2,7 +2,11 @@ package com.colphacy.repository;
 
 import com.colphacy.model.ProductImage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductImageRepository extends JpaRepository<ProductImage, Long> {
+    @Modifying
+    @Query("delete from ProductImage pi where pi.product.id = ?1")
     void deleteByProductId(Long productId);
 }
