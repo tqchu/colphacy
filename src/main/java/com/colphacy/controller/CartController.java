7efe42bc4072066fa92012143a8eccf88472362f
spartan-duyle <duy.le@c0x12c.com>
2023-11-15 22:44:38 +0700

    add operation to cart management api

--- src/main/java/com/colphacy/controller/CartController.java
+++ src/main/java/com/colphacy/controller/CartController.java
@@ -5,6 +5,8 @@ import com.colphacy.dto.cartItem.CartItemListViewDTO;
 import com.colphacy.model.Customer;
 import com.colphacy.service.CartItemService;
 import com.colphacy.service.CustomerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,24 +27,28 @@ public class CartController {
         this.cartItemService = cartItemService;
     }
 
+    @Operation(summary = "Get a list of cart items", security = {@SecurityRequirement(name = "bearer-key")})
     @GetMapping()
     public List<CartItemListViewDTO> getListCartItem(Principal principal) {
         Customer customer = customerService.getCurrentlyLoggedInCustomer(principal);
         return cartItemService.findByCustomerId(customer.getId());
     }
 
+    @Operation(summary = "Add product to cart", security = {@SecurityRequirement(name = "bearer-key")})
     @PostMapping("/add")
     public void addProductToCart(@RequestBody @Valid CartItemDTO cartItemDTO, Principal principal) {
         Customer customer = customerService.getCurrentlyLoggedInCustomer(principal);
         cartItemService.addProduct(cartItemDTO.getProductId(), cartItemDTO.getQuantity(), customer);
     }
 
+    @Operation(summary = "Update quantity of product in cart", security = {@SecurityRequirement(name = "bearer-key")})
     @PutMapping("/update")
     public void updateQuantity(@RequestBody @Valid CartItemDTO cartItemDTO,  Principal principal) {
         Customer customer = customerService.getCurrentlyLoggedInCustomer(principal);
         cartItemService.updateQuantity(cartItemDTO.getProductId(), customer.getId(), cartItemDTO.getQuantity());
     }
 
+    @Operation(summary = "Remove product from cart", security = {@SecurityRequirement(name = "bearer-key")})
     @DeleteMapping("/remove/{product_id}")
     public void removeProductFromCart(@PathVariable("product_id") Long productId,  Principal principal) {
         Customer customer = customerService.getCurrentlyLoggedInCustomer(principal);
