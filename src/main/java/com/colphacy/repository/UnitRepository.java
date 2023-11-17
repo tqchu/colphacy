0a76c2caf9b9ce6ce82d739f50bb17bfb939b188
tqchu <chu.tq@teko.vn>
2023-11-18 01:36:40 +0700

    Add api get units by product

--- src/main/java/com/colphacy/repository/UnitRepository.java
+++ src/main/java/com/colphacy/repository/UnitRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -15,4 +16,7 @@ public interface UnitRepository extends JpaRepository<Unit, Long> {
 
     @Query("SELECT u FROM Unit u WHERE LOWER(unaccent(u.name)) LIKE LOWER(unaccent(concat('%', :name, '%')))")
     Page<Unit> findUnitByNameContaining(String name, Pageable pageable);
+
+    @Query("SELECT u FROM ProductUnit pu JOIN pu.unit u JOIN pu.product p WHERE p.id = :id")
+    List<Unit> findAllByProductId(Long id);
 }
