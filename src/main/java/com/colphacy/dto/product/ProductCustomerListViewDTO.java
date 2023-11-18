8ad5fc273b8464f23a582355567fd949ce33de93
tqchu <chu.tq@teko.vn>
2023-11-18 08:31:31 +0700

    Add unit id to product customers

--- src/main/java/com/colphacy/dto/product/ProductCustomerListViewDTO.java
+++ src/main/java/com/colphacy/dto/product/ProductCustomerListViewDTO.java
@@ -7,6 +7,7 @@ public class ProductCustomerListViewDTO {
     private Long id;
     private String name;
     private Double salePrice;
+    private Long unitId;
     private String unitName;
     private String image;
 }
