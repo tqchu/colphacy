fec9e07f088f92448d09976cf23171c58cfadfff
spartan-duyle <93651748+spartan-duyle@users.noreply.github.com>
2023-11-25 13:24:41 +0700

    Cp 174 create a new order (#45)

--- src/main/java/com/colphacy/model/Order.java
+++ src/main/java/com/colphacy/model/Order.java
@@ -29,7 +29,7 @@ public class Order {
     private Receiver receiver;
 
     @NotNull
-    private LocalDateTime orderTime;
+    private LocalDateTime orderTime = LocalDateTime.now();
     private LocalDateTime confirmTime;
     private LocalDateTime shipTime;
     private LocalDateTime deliverTime;
