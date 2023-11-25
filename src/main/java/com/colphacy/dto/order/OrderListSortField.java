e0ca985ca919ffbe16f82309dc41267a2e67fc85
Trương Quang Chứ <truongquangchu.tqc@gmail.com>
2023-11-25 11:11:15 +0700

    CP-199: Get list orders by status (#44)
    Co-authored-by: tqchu <chu.tq@teko.vn>

---
+++ src/main/java/com/colphacy/dto/order/OrderListSortField.java
@@ -0,0 +1,5 @@
+package com.colphacy.dto.order;
+
+public enum OrderListSortField {
+    TOTAL, TIME, ORDER_TIME, DELIVER_TIME, CONFIRM_TIME, SHIP_TIME, CANCEL_TIME
+}
--- src/main/java/com/colphacy/service/OrderService.java
+++ src/main/java/com/colphacy/service/OrderService.java
@@ -2,8 +2,13 @@ package com.colphacy.service;
 
 import com.colphacy.dto.order.OrderCreateDTO;
 import com.colphacy.dto.order.OrderDTO;
+import com.colphacy.dto.order.OrderListViewDTO;
+import com.colphacy.dto.order.OrderSearchCriteria;
 import com.colphacy.model.Customer;
+import com.colphacy.payload.response.PageResponse;
 
 public interface OrderService {
     OrderDTO createOrder(OrderCreateDTO orderCreateDTO, Customer customer);
+
+    PageResponse<OrderListViewDTO> getPaginatedOrders(OrderSearchCriteria criteria);
 }
