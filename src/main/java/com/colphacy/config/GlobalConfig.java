d1a4c6ff363be258c60aecc683b7c3765c4edb37
tqchu <chu.tq@teko.vn>
2023-11-26 22:02:02 +0700

    Allow all domains

--- src/main/java/com/colphacy/config/GlobalConfig.java
+++ src/main/java/com/colphacy/config/GlobalConfig.java
@@ -33,7 +33,7 @@ public class GlobalConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "http://localhost:5174", "http://localhost:3000", "http://localhost:3001", "http://localhost:8081", "https://colphacy-user-client.vercel.app", "https://colphacy-admin-client.vercel.app", "https://colphacy-user-client-git-master-kaice88s-projects.vercel.app", "https://colphacy-user-client-cy6ozl5p5-kaice88s-projects.vercel.app")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
             }
         };
