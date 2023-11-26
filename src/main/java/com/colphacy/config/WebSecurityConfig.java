db821464e47c035d652c25b717482843b03f6a2e
tqchu <chu.tq@teko.vn>
2023-11-26 20:07:21 +0700

    Change to use purchase endpoint

--- src/main/java/com/colphacy/config/WebSecurityConfig.java
+++ src/main/java/com/colphacy/config/WebSecurityConfig.java
@@ -75,6 +75,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/docs").permitAll()
                 .antMatchers("/api/swagger-ui/**").permitAll()
                 .antMatchers("/api/docs.json/**").permitAll()
+                .antMatchers(("/api/orders/purchase")).hasAuthority("CUSTOMER")
                 .antMatchers(("/api/products/best-sellers")).permitAll()
                 .antMatchers(("/api/products/customers/**")).permitAll()
                 .antMatchers("/api/auth/employee/logout").hasAnyAuthority("STAFF", "ADMIN")
