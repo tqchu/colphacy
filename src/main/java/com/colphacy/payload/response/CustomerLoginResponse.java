f9a771d38e8713ab60220278469c7c0e874fc01d
tqchu <chu.tq@teko.vn>
2023-11-28 22:58:49 +0700

    Return expirationTime in login response

--- src/main/java/com/colphacy/payload/response/CustomerLoginResponse.java
+++ src/main/java/com/colphacy/payload/response/CustomerLoginResponse.java
@@ -5,10 +5,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CustomerLoginResponse {
     private CustomerDetailDTO userProfile;
     private String accessToken;
+    private LocalDateTime expirationTime;
 }
