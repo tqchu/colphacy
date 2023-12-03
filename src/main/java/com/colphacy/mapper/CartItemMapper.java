6f75f8eb20f2eae9982de31b0382e4f0bbde9e5b
spartan-duyle <93651748+spartan-duyle@users.noreply.github.com>
2023-12-03 15:21:27 +0700

    Fix cart item mapper (#50)

--- src/main/java/com/colphacy/mapper/CartItemMapper.java
+++ src/main/java/com/colphacy/mapper/CartItemMapper.java
@@ -1,12 +1,47 @@
 package com.colphacy.mapper;
 
 import com.colphacy.dto.cart.CartItemListViewDTO;
+import com.colphacy.dto.product.ProductCustomerListViewDTO;
+import com.colphacy.exception.RecordNotFoundException;
 import com.colphacy.model.CartItem;
+import com.colphacy.model.Product;
+import com.colphacy.model.ProductUnit;
+import com.colphacy.model.Unit;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring", uses = {ProductMapper.class})
+@Mapper(componentModel = "spring")
 public interface CartItemMapper {
-    @Mapping(source = "product", target = "productInfo")
-    CartItemListViewDTO cartItemToCartItemListViewDTO(CartItem cartItem);
+    default CartItemListViewDTO cartItemToCartItemListViewDTO(CartItem cartItem) {
+        if ( cartItem == null ) {
+            return null;
+        }
+
+        CartItemListViewDTO cartItemListViewDTO = new CartItemListViewDTO();
+
+        cartItemListViewDTO.setProductInfo( productAndUnitToProductCustomerListViewDTO( cartItem.getProduct(), cartItem.getUnit() ) );
+        cartItemListViewDTO.setId( cartItem.getId() );
+        cartItemListViewDTO.setQuantity( cartItem.getQuantity() );
+
+        return cartItemListViewDTO;
+    }
+
+    default ProductCustomerListViewDTO productAndUnitToProductCustomerListViewDTO(Product product, Unit unit) {
+        ProductCustomerListViewDTO res = new ProductCustomerListViewDTO();
+        res.setName(product.getName());
+
+        if (!product.getImages().isEmpty()) {
+            res.setImage(product.getImages().get(0).getUrl());
+        }
+
+        ProductUnit productUnit = product.getProductUnits().stream()
+                .filter(pu -> pu.getUnit().equals(unit))
+                .findFirst().get();
+
+        res.setUnitName(productUnit.getUnit().getName());
+        res.setUnitId(productUnit.getUnit().getId());
+        res.setSalePrice(productUnit.getSalePrice());
+
+        res.setId(product.getId());
+        return res;
+    }
 }
--- src/main/java/com/colphacy/mapper/ProductMapper.java
+++ src/main/java/com/colphacy/mapper/ProductMapper.java
@@ -1,13 +1,9 @@
 package com.colphacy.mapper;
 
-import com.colphacy.dto.product.ProductCustomerListViewDTO;
 import com.colphacy.dto.product.ProductDTO;
 import com.colphacy.dto.product.ProductSimpleDTO;
-import com.colphacy.exception.RecordNotFoundException;
 import com.colphacy.model.Product;
 import com.colphacy.model.ProductImage;
-import com.colphacy.model.ProductUnit;
-import com.colphacy.model.Unit;
 import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -29,43 +25,6 @@ public interface ProductMapper {
         if (product.getImages() != null && !product.getImages().isEmpty()) {
             productSimpleDTO.setImage(product.getImages().get(0).getUrl());
         }
-    }
-
-    default ProductCustomerListViewDTO productToProductCustomerListViewDTO(Product product) {
-        ProductCustomerListViewDTO res = new ProductCustomerListViewDTO();
-        res.setName(product.getName());
-
-        if (!product.getImages().isEmpty()) {
-            res.setImage(product.getImages().get(0).getUrl());
-        }
-
-        if (!product.getProductUnits().isEmpty()) {
-            res.setUnitName(product.getProductUnits().get(0).getUnit().getName());
-            res.setSalePrice(product.getProductUnits().get(0).getSalePrice());
-        }
-
-        res.setId(product.getId());
-        return res;
-    }
-
-    default ProductCustomerListViewDTO productAndUnitToProductCustomerListViewDTO(Product product, Unit unit) {
-        ProductCustomerListViewDTO res = new ProductCustomerListViewDTO();
-        res.setName(product.getName());
-
-        if (!product.getImages().isEmpty()) {
-            res.setImage(product.getImages().get(0).getUrl());
-        }
-
-        ProductUnit productUnit = product.getProductUnits().stream()
-                .filter(pu -> pu.getUnit().equals(unit))
-                .findFirst()
-                .orElseThrow(() -> new RecordNotFoundException("Không tìm thấy đơn vị"));
-
-        res.setUnitName(productUnit.getUnit().getName());
-        res.setSalePrice(productUnit.getSalePrice());
-
-        res.setId(product.getId());
-        return res;
     }
 
     default String fromProductImage(ProductImage productImage) {
