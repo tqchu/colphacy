bc8d8c23744dea07c352a70130304e77b59902c0
spartan-duyle <93651748+spartan-duyle@users.noreply.github.com>
2023-12-07 10:13:09 +0700

    CP 228 be admin get the all reviews (#54)

--- src/main/java/com/colphacy/dto/product/ProductSimpleDTO.java
+++ src/main/java/com/colphacy/dto/product/ProductSimpleDTO.java
@@ -1,11 +1,15 @@
 package com.colphacy.dto.product;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductSimpleDTO {
 
     @NotNull(message = "Phải có thông tin về sản phẩm")
--- src/main/java/com/colphacy/repository/ReviewRepository.java
+++ src/main/java/com/colphacy/repository/ReviewRepository.java
@@ -1,5 +1,6 @@
 package com.colphacy.repository;
 
+import com.colphacy.dto.review.ReviewAdminListViewDTO;
 import com.colphacy.model.Review;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,8 +9,6 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
@@ -40,4 +39,63 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     @Query(value = "select * from reviews r where r.parent_review_id = :parentReviewId order by r.created_time", nativeQuery = true)
     Review findByParentReviewId(Long parentReviewId);
+
+    @Query(value = """
+            SELECT r.id as id,
+                   p.id as product_id,
+                   p.name as product_name,
+                   (SELECT pi.url
+                    FROM product_image pi
+                    WHERE pi.product_id = r.product_id
+                    ORDER BY pi.id
+                    LIMIT 1) as product_image,
+                   c.full_name as customer_name,
+                   r.customer_id as customer_id,
+                   r.rating as rating,
+                   r.content as content,
+                   r.created_time as created_time,
+                   child.id as reply_review_id,
+                   child.content as reply_review_content,
+                   child.created_time as reply_review_created_time,
+                   e.id as employee_id,
+                   e.full_name as employee_name
+            FROM reviews r
+                     JOIN customer c ON r.customer_id = c.id
+                     LEFT JOIN reviews child ON r.id = child.parent_review_id
+                     LEFT JOIN employee e ON child.employee_id = e.id
+                     JOIN product p ON r.product_id = p.id
+            WHERE (unaccent(lower(r.content)) LIKE unaccent(lower('%' || :keyword || '%'))
+            OR unaccent(lower(p.name)) LIKE unaccent(lower('%' || :keyword || '%'))
+            OR unaccent(lower(c.full_name)) LIKE unaccent(lower('%' || :keyword || '%')))
+              AND r.parent_review_id is null
+            """, nativeQuery = true)
+    Page<Object[]> findAllReviewsWithKeyword(@Param("keyword") String keyword, Pageable pageable);
+
+    @Query(value = """
+                SELECT r.id as id,
+                       p.id as product_id,
+                       p.name as product_name,
+                       (SELECT pi.url
+                        FROM product_image pi
+                        WHERE pi.product_id = r.product_id
+                        ORDER BY pi.id
+                        LIMIT 1) as product_image,
+                       c.full_name as customer_name,
+                       r.customer_id as customer_id,
+                       r.rating as rating,
+                       r.content as content,
+                       r.created_time as created_time,
+                       child.id as reply_review_id,
+                       child.content as reply_review_content,
+                       child.created_time as reply_review_created_time,
+                       e.id as employee_id,
+                       e.full_name as employee_name
+                FROM reviews r
+                         JOIN customer c ON r.customer_id = c.id
+                         LEFT JOIN reviews child ON r.id = child.parent_review_id
+                         LEFT JOIN employee e ON child.employee_id = e.id
+                         JOIN product p ON r.product_id = p.id
+                WHERE r.parent_review_id IS NULL
+            """, nativeQuery = true)
+    Page<Object[]> findAllReviews(Pageable pageable);
 }
