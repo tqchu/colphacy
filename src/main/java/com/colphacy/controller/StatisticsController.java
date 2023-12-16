d231bd3e6f6124d4ae1e986601c7184d85ff5fdb
Trương Quang Chứ <truongquangchu.tqc@gmail.com>
2023-12-16 18:17:42 +0700

    Draft statistics API
    Co-authored-by: tqchu <chu.tq@teko.vn>

--- src/main/java/com/colphacy/controller/StatisticsController.java
+++ src/main/java/com/colphacy/controller/StatisticsController.java
@@ -1,8 +1,17 @@
 package com.colphacy.controller;
 
+import com.colphacy.dto.statistics.ProductsStatisticsDTO;
+import com.colphacy.service.StatisticsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.Positive;
 
 @RestController
 @RequestMapping("/api/statistics")
@@ -10,15 +19,29 @@ public class StatisticsController {
     @Value("${colphacy.api.default-page-size}")
     private Integer defaultPageSize;
 
-//    @Operation(summary = "Get number of orders with criteria", security = {@SecurityRequirement(name = "bearer-key")})
-//    @GetMapping("")
-//    public PageResponse<StockListViewDTO> getAllStock(
-//            @Valid StockSearchCriteria criteria, Principal principal
-//    ) {
-//        Employee employee = employeeService.getCurrentlyLoggedInEmployee(principal);
-//        if (criteria.getLimit() == null) {
-//            criteria.setLimit(defaultPageSize);
-//        }
-//        return stockService.getStockView(criteria, employee);
-//    }
+    @Autowired
+    private StatisticsService statisticsService;
+
+    @Operation(summary = "Get statistics of products", security = {@SecurityRequirement(name = "bearer-key")})
+    @GetMapping("/products")
+    public ProductsStatisticsDTO getAllStock(
+            @Positive(message = "Id chi nhánh không hợp lệ")
+            @RequestParam(required = false) Integer branchId
+    ) {
+        return statisticsService.getProductStatistics(branchId);
+    }
+
+    @Operation(summary = "Get statistics of products", security = {@SecurityRequirement(name = "bearer-key")})
+    @GetMapping("")
+    public StatisticsDTO getStatisticsByTime(
+            @Positive(message = "Id chi nhánh không hợp lệ")
+            @RequestParam(required = false) Integer branchId,
+            @RequestParam(required = false) Integer month,
+            @RequestParam(required = false) Integer year,
+            int timeZone
+    ) {
+        return statisticsService.getStatistics(branchId, month, year, timeZone);
+    }
+
+
 }
