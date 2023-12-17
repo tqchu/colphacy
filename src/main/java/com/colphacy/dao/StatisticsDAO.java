cb28283a9bae90738e28a4d440336cc7207a751b
tqchu <chu.tq@teko.vn>
2023-12-18 02:02:55 +0700

    Update statistics

---
+++ src/main/java/com/colphacy/dao/StatisticsDAO.java
@@ -0,0 +1,21 @@
+package com.colphacy.dao;
+
+import com.colphacy.dto.statistics.SoldProductDTO;
+import com.colphacy.dto.statistics.SoldProductsRevenueDTO;
+import com.colphacy.model.OrderStatus;
+
+import java.util.List;
+import java.util.Map;
+
+public interface StatisticsDAO {
+
+    int getNumberOfInStockProducts(Integer branchId);
+
+    int getNumberOfNearDatedProducts(Integer branchId);
+
+    List<SoldProductDTO> getSoldProducts(Integer branchId, Integer month, Integer year, String timeZone);
+
+    SoldProductsRevenueDTO getSoldProductsAndRevenue(Integer branchId, Integer month, Integer year, String timeZoneStr);
+
+    Map<OrderStatus, Integer> getOrderStatistics(Integer branchId, Integer month, Integer year, String timeZoneStr);
+}
---
+++ src/main/java/com/colphacy/dto/statistics/SoldProductsRevenueDTO.java
@@ -0,0 +1,9 @@
+package com.colphacy.dto.statistics;
+
+import lombok.Data;
+
+@Data
+public class SoldProductsRevenueDTO {
+    private Integer totalSoldProducts;
+    private Long revenue;
+}
