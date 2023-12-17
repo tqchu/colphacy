34f3cf50265c18abaa7e83d787b28420273e4079
Trương Quang Chứ <truongquangchu.tqc@gmail.com>
2023-12-18 02:05:03 +0700

    Update statistics

---
+++ src/main/java/com/colphacy/dao/StatisticsDAOImpl.java
@@ -0,0 +1,255 @@
+package com.colphacy.dao;
+
+import com.colphacy.dto.statistics.SoldProductDTO;
+import com.colphacy.dto.statistics.SoldProductsRevenueDTO;
+import com.colphacy.model.OrderStatus;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class StatisticsDAOImpl implements StatisticsDAO {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public int getNumberOfInStockProducts(Integer branchId) {
+        boolean hasBranchCondition = branchId != null;
+        String sql =
+                """
+                        WITH imported_quantity AS (SELECT pu.product_id,
+                                                          pu.unit_id,
+                                                          i.branch_id,
+                                                          id.expiration_date,
+                                                          sum(id.quantity) AS imported_quantity
+                                                   FROM import i
+                                                            JOIN import_detail id ON i.id = id.import_id
+                                                            JOIN product_unit pu ON pu.unit_id = id.unit_id AND pu.product_id = id.product_id
+                                                            JOIN branch b ON b.id = i.branch_id
+                                                            """;
+        if (hasBranchCondition) {
+            sql += " AND b.id = :branchId ";
+        }
+        sql += """
+                                                           GROUP BY pu.unit_id, i.branch_id, pu.product_id, id.expiration_date),
+                sold_quantity AS (SELECT oi.product_id,
+                                         oi.unit_id,
+                                         o.branch_id,
+                                         oi.expiration_date,
+                                         sum(oi.quantity) AS ordered_quantity
+                                  FROM orders o
+                                           JOIN order_item oi ON o.id = oi.order_id
+                                           JOIN product_unit pu ON pu.unit_id = oi.unit_id AND pu.product_id = oi.product_id
+                                           JOIN branch b ON b.id = o.branch_id
+                                  WHERE o.status <> 'CANCELLED'
+                                  """;
+        if (hasBranchCondition) {
+            sql += " AND b.id = :branchId ";
+        }
+        sql +=
+                """
+                        GROUP BY oi.product_id, o.branch_id, oi.unit_id, oi.expiration_date)
+                        SELECT COUNT(DISTINCT io.product_id)
+                        FROM imported_quantity io
+                                 LEFT JOIN sold_quantity oo
+                                           ON io.product_id = oo.product_id AND io.branch_id = oo.branch_id AND io.unit_id = oo.unit_id AND
+                                              io.expiration_date = oo.expiration_date
+                        WHERE io.expiration_date > (CURRENT_DATE + INTERVAL '3 months')
+                          AND (io.imported_quantity - COALESCE(oo.ordered_quantity, 0)) > 0;
+                        """;
+        Query query = entityManager.createNativeQuery(sql);
+        if (hasBranchCondition) {
+            query.setParameter("branchId", branchId);
+        }
+        return ((Number) query.getSingleResult()).intValue();
+    }
+
+    @Override
+    public int getNumberOfNearDatedProducts(Integer branchId) {
+        boolean hasBranchCondition = branchId != null;
+        String sql =
+                """
+                        WITH imported_quantity AS (SELECT pu.product_id,
+                                                          pu.unit_id,
+                                                          i.branch_id,
+                                                          id.expiration_date,
+                                                          sum(id.quantity) AS imported_quantity
+                                                   FROM import i
+                                                            JOIN import_detail id ON i.id = id.import_id
+                                                            JOIN product_unit pu ON pu.unit_id = id.unit_id AND pu.product_id = id.product_id
+                                                            JOIN branch b ON b.id = i.branch_id
+                                                            """;
+        if (hasBranchCondition) {
+            sql += " AND b.id = :branchId ";
+        }
+        sql += """
+                                                           GROUP BY pu.unit_id, i.branch_id, pu.product_id, id.expiration_date),
+                sold_quantity AS (SELECT oi.product_id,
+                                         oi.unit_id,
+                                         o.branch_id,
+                                         oi.expiration_date,
+                                         sum(oi.quantity) AS ordered_quantity
+                                  FROM orders o
+                                           JOIN order_item oi ON o.id = oi.order_id
+                                           JOIN product_unit pu ON pu.unit_id = oi.unit_id AND pu.product_id = oi.product_id
+                                           JOIN branch b ON b.id = o.branch_id
+                                  WHERE o.status <> 'CANCELLED'
+                                  """;
+        if (hasBranchCondition) {
+            sql += " AND b.id = :branchId ";
+        }
+        sql +=
+                """
+                        GROUP BY oi.product_id, o.branch_id, oi.unit_id, oi.expiration_date)
+                        SELECT COUNT(DISTINCT (io.product_id, io.expiration_date))
+                        FROM imported_quantity io
+                                    LEFT JOIN sold_quantity oo
+                                           ON io.product_id = oo.product_id AND io.branch_id = oo.branch_id AND io.unit_id = oo.unit_id AND
+                                              io.expiration_date = oo.expiration_date
+                        WHERE io.expiration_date < (CURRENT_DATE + INTERVAL '3 months')
+                          AND (io.imported_quantity - COALESCE(oo.ordered_quantity, 0)) > 0
+                        """;
+        Query query = entityManager.createNativeQuery(sql);
+        if (hasBranchCondition) {
+            query.setParameter("branchId", branchId);
+        }
+        return ((Number) query.getSingleResult()).intValue();
+    }
+
+    @Override
+    public List<SoldProductDTO> getSoldProducts(Integer branchId, Integer month, Integer year, String timeZone) {
+        String sql =
+                """
+                        WITH
+                             sold_quantity AS (SELECT oi.product_id,
+                                                      oi.unit_id,
+                                                      o.branch_id,
+                                                      oi.expiration_date,
+                                                      sum(oi.quantity) AS ordered_quantity
+                                               FROM orders o
+                                                        JOIN order_item oi ON o.id = oi.order_id
+                                                        JOIN product_unit pu ON pu.unit_id = oi.unit_id AND pu.product_id = oi.product_id
+                                                        JOIN branch b ON b.id = o.branch_id
+                                               WHERE o.status <> 'CANCELLED'
+                                                    AND EXTRACT(YEAR FROM (order_time AT TIME ZONE 'UTC') AT TIME ZONE :timeZone) = :year
+                                               """;
+        if (month != null) {
+            sql += " AND EXTRACT(MONTH FROM (order_time AT TIME ZONE 'UTC') AT TIME ZONE :timeZone) = :month ";
+        }
+        if (branchId != null) {
+            sql += " AND b.id = :branchId ";
+        }
+        sql +=
+                """
+                        GROUP BY oi.product_id, o.branch_id, oi.unit_id, oi.expiration_date)
+                        SELECT id, sold, name, image
+                                FROM (
+                                         SELECT DISTINCT ON (oo.product_id)
+                                             oo.product_id as id,
+                                             ordered_quantity as sold,
+                                             p.name as name,
+                                             pi.url as image
+                                         FROM sold_quantity oo
+                                                  JOIN product p ON p.id = oo.product_id
+                                                  JOIN product_image pi ON pi.product_id = p.id
+                                         ORDER BY oo.product_id, pi.url
+                                     ) sub
+                                ORDER BY sold DESC
+                                LIMIT 5;
+                        """;
+        Query query = entityManager.createNativeQuery(sql);
+        query.setParameter("year", year);
+        query.setParameter("timeZone", timeZone);
+        if (month != null)
+            query.setParameter("month", month);
+        if (branchId != null)
+            query.setParameter("branchId", branchId);
+        return query.unwrap(org.hibernate.query.Query.class)
+                .setResultTransformer(new AliasToBeanResultTransformer(SoldProductDTO.class))
+                .getResultList();
+    }
+
+    @Override
+    public SoldProductsRevenueDTO getSoldProductsAndRevenue(Integer branchId, Integer month, Integer year, String timeZoneStr) {
+        String sql =
+                """
+                        WITH sold_quantity AS (SELECT oi.product_id,
+                                                      oi.unit_id,
+                                                      o.branch_id,
+                                                      oi.expiration_date,
+                                                      sum(oi.quantity) AS ordered_quantity,
+                                                      sum(oi.quantity*oi.price) as total_price
+                                               FROM orders o
+                                                        JOIN order_item oi ON o.id = oi.order_id
+                                                        JOIN product_unit pu ON pu.unit_id = oi.unit_id AND pu.product_id = oi.product_id
+                                                        JOIN branch b ON b.id = o.branch_id
+                                               WHERE o.status <> 'CANCELLED'
+                                                 AND EXTRACT(YEAR FROM (order_time AT TIME ZONE 'UTC') AT TIME ZONE :timeZone) = :year
+                        """;
+        if (month != null) {
+            sql += " AND EXTRACT(MONTH FROM (order_time AT TIME ZONE 'UTC') AT TIME ZONE :timeZone) = :month ";
+        }
+        if (branchId != null) {
+            sql += "  AND b.id = :branchId ";
+        }
+        sql +=
+                """
+                                               GROUP BY oi.product_id, o.branch_id, oi.unit_id, oi.expiration_date)
+                        SELECT COUNT(DISTINCT oo.product_id) as total_sold_products,COALESCE(SUM(total_price), 0) as revenue
+                        FROM sold_quantity oo
+                        """;
+
+        Query query = entityManager.createNativeQuery(sql);
+        query.setParameter("year", year);
+        query.setParameter("timeZone", timeZoneStr);
+        if (month != null)
+            query.setParameter("month", month);
+        if (branchId != null)
+            query.setParameter("branchId", branchId);
+        return (SoldProductsRevenueDTO) query.unwrap(org.hibernate.query.Query.class)
+                .setResultTransformer(new AliasToBeanResultTransformer(SoldProductsRevenueDTO.class))
+                .getSingleResult();
+
+    }
+
+    @Override
+    public Map<OrderStatus, Integer> getOrderStatistics(Integer branchId, Integer month, Integer year, String timeZoneStr) {
+        String sql =
+                """
+                        SELECT o.status, COUNT(DISTINCT  o.id)
+                        FROM orders o
+                        WHERE EXTRACT(YEAR
+                                             FROM
+                                             (order_time AT TIME ZONE 'UTC') AT TIME ZONE :timeZone) = :year
+                        """;
+        if (branchId != null) {
+            sql += " AND o.branch_id = :branchId ";
+        }
+        if (month != null) {
+            sql += " AND EXTRACT(MONTH FROM (order_time AT TIME ZONE 'UTC') AT TIME ZONE :timeZone) = :month ";
+        }
+        sql += """
+                        GROUP BY o.status
+                """;
+        Query query = entityManager.createNativeQuery(sql);
+        query.setParameter("year", year);
+        query.setParameter("timeZone", timeZoneStr);
+        if (month != null)
+            query.setParameter("month", month);
+        if (branchId != null)
+            query.setParameter("branchId", branchId);
+
+        List<Object[]> results = query.getResultList();
+        Map<OrderStatus, Integer> statistics = new HashMap<>();
+        for (Object[] result : results) {
+            statistics.put(OrderStatus.valueOf((String) result[0]), ((Number) result[1]).intValue());
+        }
+        return statistics;
+
+
+    }
+}
--- src/main/java/com/colphacy/service/impl/StatisticsServiceImpl.java
+++ src/main/java/com/colphacy/service/impl/StatisticsServiceImpl.java
@@ -1,5 +1,6 @@
 package com.colphacy.service.impl;
 
+import com.colphacy.dao.StatisticsDAO;
 import com.colphacy.dto.statistics.*;
 import com.colphacy.exception.InvalidFieldsException;
 import com.colphacy.model.OrderStatus;
@@ -12,7 +13,6 @@ import org.springframework.stereotype.Service;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -24,16 +24,22 @@ public class StatisticsServiceImpl implements StatisticsService {
     @Autowired
     private ImportRepository importRepository;
 
+    @Autowired
+    private StatisticsDAO statisticsDAO;
+
     @Override
     public ProductsStatisticsDTO getProductStatistics(Integer branchId) {
+        int inStockProducts = statisticsDAO.getNumberOfInStockProducts(branchId);
+        int nearDated = statisticsDAO.getNumberOfNearDatedProducts(branchId);
         ProductsStatisticsDTO result = new ProductsStatisticsDTO();
-        result.setInStock(10);
-        result.setNearDated(15);
+        result.setInStock(inStockProducts);
+        result.setNearDated(nearDated);
         return result;
     }
 
     @Override
     public StatisticsDTO getStatistics(Integer branchId, Integer month, Integer year, int timeZone) {
+        String timeZoneStr = (timeZone > 0 ? "+" : "") + timeZone;
         // Validate branch
         if (branchId != null) {
             branchService.findBranchById(branchId);
@@ -61,56 +67,28 @@ public class StatisticsServiceImpl implements StatisticsService {
             }
 
         }
-        System.out.println(month);
-        System.out.println(year);
         StatisticsDTO result = new StatisticsDTO();
-        List<SoldProductDTO> products = new ArrayList<>();
-        SoldProductDTO product1 = new SoldProductDTO();
-        product1.setId(1);
-        product1.setName("Product 1");
-        product1.setImage("https://kenh14cdn.com/thumb_w/660/2017/10-1513091306683.png");
-        product1.setSold(10);
-        SoldProductDTO product2 = new SoldProductDTO();
-        product2.setId(2);
-        product2.setName("Kim Chi");
-        product2.setImage("https://colphacyy.blob.core.windows.net/colphacy/kc.jpg_deb03d7b-41e4-4e79-aba1-06e23893aea7");
-        product2.setSold(15);
-        SoldProductDTO product3 = new SoldProductDTO();
-        product3.setId(3);
-        product3.setName("Product 3");
-        product3.setImage("https://kenh14cdn.com/thumb_w/660/2017/10-1513091306683.png");
-        product3.setSold(21);
-        SoldProductDTO product4 = new SoldProductDTO();
-        product4.setId(4);
-        product4.setName("Product 4");
-        product4.setImage("https://kenh14cdn.com/thumb_w/660/2017/10-1513091306683.png");
-        product4.setSold(31);
-        SoldProductDTO product5 = new SoldProductDTO();
-        product5.setId(5);
-        product5.setName("Product 5");
-        product5.setImage("https://kenh14cdn.com/thumb_w/660/2017/10-1513091306683.png");
-        product5.setSold(51);
-        products.add(product1);
-        products.add(product2);
-        products.add(product3);
-        products.add(product4);
-        products.add(product5);
+        List<SoldProductDTO> products = statisticsDAO.getSoldProducts(branchId, month, year, timeZoneStr);
         result.setProducts(products);
 
         OrderStatisticsDTO orders = new OrderStatisticsDTO();
-        orders.setRevenue(30000000);
-        orders.setTotalNumOrders(90);
-        orders.setSoldProducts(30);
+        SoldProductsRevenueDTO soldProductsRevenueDTO = statisticsDAO.getSoldProductsAndRevenue(branchId, month, year, timeZoneStr);
+        orders.setRevenue(soldProductsRevenueDTO.getRevenue());
+        orders.setSoldProducts(soldProductsRevenueDTO.getTotalSoldProducts());
         orders.setOrderChangePercent(10.5);
         orders.setRevenueChangePercent(-12.5);
         orders.setSoldProductChangePercent(0);
-        Map<OrderStatus, Integer> orderNumMaps = new HashMap<>();
-        orderNumMaps.put(OrderStatus.PENDING, 10);
-        orderNumMaps.put(OrderStatus.CONFIRMED, 15);
-        orderNumMaps.put(OrderStatus.SHIPPING, 20);
-        orderNumMaps.put(OrderStatus.DELIVERED, 25);
-        orderNumMaps.put(OrderStatus.CANCELLED, 20);
+
+
+        Map<OrderStatus, Integer> orderNumMaps = statisticsDAO.getOrderStatistics(branchId, month, year, timeZoneStr);
+        for (OrderStatus orderStatus : OrderStatus.values()) {
+            if (!orderNumMaps.containsKey(orderStatus)) {
+                orderNumMaps.put(orderStatus, 0);
+            }
+        }
+
         orders.setNumOrdersMap(orderNumMaps);
+        orders.setTotalNumOrders(orderNumMaps.values().stream().mapToInt(Integer::intValue).sum());
         result.setOrders(orders);
 
         ImportRevenueStatisticsDTO pnl = new ImportRevenueStatisticsDTO();
@@ -137,6 +115,7 @@ public class StatisticsServiceImpl implements StatisticsService {
 
     @Override
     public List<Integer> getAvailableYears(int timeZone) {
+        // TODO: update the query: must use DISTINCT (union of (import and order) + current year)
         String timeZoneStr = (timeZone > 0 ? "+" : "") + timeZone;
         return importRepository.getAvailableYear(timeZoneStr);
     }
