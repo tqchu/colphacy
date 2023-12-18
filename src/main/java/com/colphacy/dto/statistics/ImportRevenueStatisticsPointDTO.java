ebd355d683c271b3a1c853a7bea916b0f54cdb18
tqchu <chu.tq@teko.vn>
2023-12-18 10:45:53 +0700

    Add revenue import amount

--- src/main/java/com/colphacy/dto/statistics/ImportRevenueStatisticsPointDTO.java
+++ src/main/java/com/colphacy/dto/statistics/ImportRevenueStatisticsPointDTO.java
@@ -1,10 +1,8 @@
 package com.colphacy.dto.statistics;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class ImportRevenueStatisticsPointDTO {
     private long revenue;
     private long importAmount;
