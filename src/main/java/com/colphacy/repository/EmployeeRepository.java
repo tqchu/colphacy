f12ce4c587cb180db37edd6e8d4bcd09da93533b
Trương Quang Chứ <truongquangchu.tqc@gmail.com>
2023-12-25 22:39:55 +0700

    CP-262: Add notifications when customer purchasing orders (#72)
    Co-authored-by: tqchu <chu.tq@teko.vn>

--- src/main/java/com/colphacy/repository/EmployeeRepository.java
+++ src/main/java/com/colphacy/repository/EmployeeRepository.java
@@ -3,8 +3,10 @@ package com.colphacy.repository;
 import com.colphacy.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -14,4 +16,12 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long>, JpaSp
     Optional<Employee> findByPhone(String phone);
 
     boolean existsByBranchId(Long id);
+
+    @Query("""
+            SELECT e
+            FROM Employee e
+            WHERE e.branch.id IS null OR e.branch.id = :branchId
+            """
+    )
+    List<Employee> findEmployeeByOfABranch(Long branchId);
 }
